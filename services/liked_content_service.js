73b870d910e825b4b6dee0c78d8481b1a40583c1
Sumit <sum.itvoid00@gmail.com>
2025-02-15 11:23:31 +0530

    Update liked_content_service.js

--- services/liked_content_service.js
+++ services/liked_content_service.js
@@ -1,9 +1,8 @@
-// liked_content_service.js
 const Like = require('../models/like');
 const Content = require('../models/content');
 const { NotFoundError } = require('../utils/errors');
 const mongoose = require('mongoose');
-// Get all liked video content for a user
+
 async function getUserLikedContent(userId) {
     try {
         if (!mongoose.Types.ObjectId.isValid(userId)) {
